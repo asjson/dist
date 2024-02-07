d863d32e07e9d7a955967fd50aec605ceb48fc17
asjson <1432354487@qq.com>
2024-02-08 02:39:08 +0800

    fix bug

---
+++ static/txt.17e9a01b.js
@@ -0,0 +1,1 @@
+import{I as e}from"./index.eaf967b7.js";let r="/pc/assist/get";function p(t){return e({url:r,method:"post",data:t,headers:{"Content-Type":"application/json"}})}export{p as t};
--- static/txt.27bdb742.js
+++
@@ -1,1 +0,0 @@
-import{I as e}from"./index.01cc0584.js";let r="/pc/assist/get";function p(t){return e({url:r,method:"post",data:t,headers:{"Content-Type":"application/json"}})}export{p as t};
